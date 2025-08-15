4ccabd7853c99dfa91c1f1bc545d1ea4c94b81f6
Thiago <tjose.corp@gmail.com>
2025-08-15 17:56:06 -0300

    Create People class to store name, age, and height
    - Added fields: name, age, height
    - Made fields final for immutability
    - Added constructor to initialize all fields
    - Added getters for name, age, and height

---
+++ src/entities/People.java
@@ -0,0 +1,26 @@
+package entities;
+
+public class People {
+
+    private final String name;
+    private final int age;
+    private final double height;
+
+    public People(String name, int age, double height) {
+        this.name = name;
+        this.age = age;
+        this.height = height;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public double getHeight() {
+        return height;
+    }
+}
